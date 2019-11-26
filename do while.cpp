02e86a5ca9272a8db6cf50df201d5bbcdbcdd111
shantinghsu <54939923+shantinghsu@users.noreply.github.com>
2019-11-26 20:24:03 +0800

    new  endl
    endl = end line

---
+++ do while.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+  int height;
+
+  do {
+      cout << "請輸入高度:";
+      cin >> height;
+  } while (-1 >= height > 8); 
+  do {
+      cout << "請輸入高度:";
+      cin >> height ;
+  } while (8 < height);
+
+  for (int i = 1; i <= height; i++) {
+      for (int j = 1; j <= height; j++) {
+          if (j <= (height - i)) {
+              cout << " ";
+          } else {
+              cout << "*";
+          }
+      } cout << endl;
+  }
+}
