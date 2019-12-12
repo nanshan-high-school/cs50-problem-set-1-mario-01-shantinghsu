021fb34ee412b559553ad7f6000916248cecbac2
shantinghsu <54939923+shantinghsu@users.noreply.github.com>
2019-12-12 19:36:21 +0800

    Update do while.cpp

--- do while.cpp
+++ do while.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
-
 using namespace std;
-int main(void) {
-  int height;
 
-  do {
-      cout << "請輸入高度:";
-      cin >> height;
-  } while (-1 >= height > 8); 
-  do {
-      cout << "請輸入高度:";
-      cin >> height ;
-  } while (8 < height);
+int main() {
+    int money;
 
-  for (int i = 1; i <= height; i++) {
-      for (int j = 1; j <= height; j++) {
-          if (j <= (height - i)) {
-              cout << " ";
-          } else {
-              cout << "#";
-          }
-      } cout << endl;
-  }
+    do {
+        cout << "請輸入金額:";
+        cin >> money;
+
+    } while (money < 0);
+        if (money >= 1000) {
+            cout << money / 1000  << "張一千元" << "\n";
+            money -= 1000 * (money / 1000);
+        } else if (money >= 500) {
+            cout << money / 500  << "張五百元" << "\n";
+            money -= 500 * (money / 500);  
+        } else if (money >= 100) {
+            cout << money / 100  << "張一百元" << "\n";
+            money -= 100 * (money / 100);
+        } else if (money >= 50) {
+            cout << money / 50  << "個五十元" << "\n";
+            money -= 50 * (money / 50);
+        } else if (money >= 10) {
+            cout << money / 10  << "個十元" << "\n";
+            money -= 10 * (money / 10);
+        } else if (money >= 5) {
+            cout << money / 5  << "個五元" << "\n";
+            money -= 5 * (money / 5);
+        } else {
+            cout << money / 1 << "個一元" << "\n";
+            money -= 1 * (money / 1);
+        } 
+    
 }
